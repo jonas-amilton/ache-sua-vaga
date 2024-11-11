e922b45bc4014b2c865872e7eba36386f1c66b2d
jonas-amilton <jonas96@edu.unisinos.br>
2024-11-10 22:54:41 -0300

    chore: subindo uma versão de exemplo do main-local.php para o repositório remoto, com a configuração do .env

---
+++ common/config/main-local.example.php
@@ -0,0 +1,46 @@
+<?php
+// Loading environment variables
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+return [
+    'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'dsn' => 'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'],
+            'username' => $_ENV['DB_USER'],
+            'password' => $_ENV['DB_PASSWORD'],
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default.
+            'useFileTransport' => true,
+            // You have to set
+            //
+            // 'useFileTransport' => false,
+            //
+            // and configure a transport for the mailer to send real emails.
+            //
+            // SMTP server example:
+            //    'transport' => [
+            //        'scheme' => 'smtps',
+            //        'host' => '',
+            //        'username' => '',
+            //        'password' => '',
+            //        'port' => 465,
+            //        'dsn' => 'native://default',
+            //    ],
+            //
+            // DSN example:
+            //    'transport' => [
+            //        'dsn' => 'smtp://user:pass@smtp.example.com:25',
+            //    ],
+            //
+            // See: https://symfony.com/doc/current/mailer.html#using-built-in-transports
+            // Or if you use a 3rd party service, see:
+            // https://symfony.com/doc/current/mailer.html#using-a-3rd-party-transport
+        ],
+    ],
+];
